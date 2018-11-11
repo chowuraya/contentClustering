d0c6a97c2100cc4698379f18efcf47923fac990c
Tawanda <tawandachowuraya@yahoo.co.uk>
2018-11-11 05:45:36 -0800

    running but no records

--- main/main.go
+++ main/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type myData struct {
+	myNumber   string `json:"Number"`
+	myUrl      string `json:"URL"`
+	myCategory string `json:"Category"`
+	myLabel    string `json:"Label"`
+	mydate     string `json:"Date"`
+}
 
 func main() {
 	fmt.Print("welcome hi")
+	csvFile, _ := os.Open("NewsURLs.csv")
+	reader := csv.NewReader(csvFile)
+	var theData []myData
+
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		}
+		if error != nil {
+			log.Fatal(error)
+		}
+		theData = append(theData, myData{
+			myNumber:   line[0],
+			myUrl:      line[1],
+			myCategory: line[2],
+			myLabel:    line[3],
+			mydate:     line[4],
+		})
+	}
+	theDataJson, _ := json.Marshal(theData)
+	fmt.Println(string(theDataJson))
 }
